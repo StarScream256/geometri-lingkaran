250b367003594f2065621fb4bab77ebb4ab00b21
dew <devio@basrengganteng>
2026-05-03 23:14:37 +0700

    Bola n Juring v1

--- src/geometri3d/JuringBola.java
+++ src/geometri3d/JuringBola.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package geometri3d;
 
-/**
- *
- * @author devio
- */
-public class JuringBola {
+public class JuringBola extends Bola {
+    private double tinggiJuring;
     
+    public JuringBola(double r, double tinggiJuring){
+        super(r);
+        this.tinggiJuring = tinggiJuring;
+    }
+    //A adalah jari-jari alas juring
+    public double hitungA(){
+        double r = getJariJari();
+        return Math.sqrt(Math.pow(r,2) - Math.pow(r-tinggiJuring,2));
+    }
+    @Override
+    public double hitungVolume(){
+        double r = getJariJari();
+        return (2.0/3.0) * Math.PI * Math.pow(r,2) * tinggiJuring;
+    }
+    
+    @Override
+    public double hitungLuasPermukaan(){
+        double a = hitungA();
+        double r = getJariJari();
+        return (2* Math.PI *r * tinggiJuring) + (Math.PI * r * a);
+    }
 }
