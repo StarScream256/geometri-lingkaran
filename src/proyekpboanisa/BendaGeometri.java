6b91f95933aaf39a0f2ea31bc60d9dd9d8dad894
Anisa zahida <azazahida35@gmail.com>
2026-05-03 21:09:19 +0700

    Add files via upload

---
+++ src/proyekpboanisa/BendaGeometri.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyekpboanisa;
+
+/**
+ *
+ * @author Lenovo
+ */
+public abstract class BendaGeometri {
+    public abstract double hitungLuas();
+    public abstract double hitungKeliling();
+}
